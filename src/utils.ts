6e02a132a103caa4f7e7bb292b6dfe45f2c014c2
Milan <milanvodak02@gmail.com>
2021-12-12 16:26:29 +0100

    Extract directions to constant

--- src/utils.ts
+++ src/utils.ts
@@ -3,14 +3,15 @@ import Wall from "./wall";
 
 type Orientation = 'H' | 'V';
 type Direction = { name: 'N' | 'E' | 'S' | 'W', x: -1 | 0 | 1, y: -1 | 0 | 1 };
+export const directions: Direction[] = [
+  { name: 'N', x: 0, y: -1 },
+  { name: 'E', x: 1, y: 0 },
+  { name: 'S', x: 0, y: 1 },
+  { name: 'W', x: -1, y: 0 },
+];
 
 export function getRandomDirection() {
-  return [
-    { name: 'N', x: 0, y: -1 },
-    { name: 'E', x: 1, y: 0 },
-    { name: 'S', x: 0, y: 1 },
-    { name: 'W', x: -1, y: 0 },
-  ][Math.floor(Math.random() * 4)] as Direction;
+  return directions[Math.floor(Math.random() * directions.length)];
 }
 
 export function getRandomEdgeTile(a: Tile[][], direction: Direction) {
@@ -26,6 +27,22 @@ export function getRandomEdgeTile(a: Tile[][], direction: Direction) {
     default:
       return a[index][0];
   }
+}
+
+export function canMove(start: Tile, direction: Direction, walls: Wall[]) {
+  const destination = { x: start.x + direction.x, y: start.y + direction.y };
+  return !walls.some(wall => {
+    switch (direction.name) {
+      case 'N':
+        return wall.x === start.x && wall.y === start.y && wall.direction === 'E';
+      case 'E':
+        return wall.x === destination.x && wall.y === destination.y && wall.direction === 'S';
+      case 'S':
+        return wall.x === destination.x && wall.y === destination.y && wall.direction === 'E';
+      default:
+        return wall.x === start.x && wall.y === start.y && wall.direction === 'S';
+    }
+  })
 }
 
 export function splitField(a: Tile[][], orientation: Orientation, index: number) {
