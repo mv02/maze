ad53df0b2c5b4198f8ff4620840009753ec8942b
Milan <milanvodak02@gmail.com>
2021-12-14 12:10:53 +0100

    Create a dynamic UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,59 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  import NumberInput from './NumberInput.svelte';
   import Maze from './maze';
 
+  let size: number = 20;
+  let maze: Maze;
+  let solved: boolean = false;
+
+  let container: Element;
   let canvas: HTMLCanvasElement;
+  let ctx: CanvasRenderingContext2D;
+
   onMount(() => {
-    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-  
-    const m = new Maze();
-    m.draw(ctx);
-    m.solve();
-    m.drawSolution(ctx);
+    ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    generate();
   });
+
+  function generate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.width = canvas.height = Math.min(
+      document.documentElement.clientWidth,
+      document.documentElement.clientHeight
+    ) - 2 * +window.getComputedStyle(container).padding.replace('px', '');
+
+    maze = new Maze(size, canvas.width / size, canvas.width / size / 5);
+    solved = false;
+    maze.draw(ctx);
+    maze.solve();
+  }
+
+  $: if (maze) {
+    try {
+      solved ? maze.drawSolution(ctx) : maze.draw(ctx);
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
 </script>
 
-<div class="container flex items-center justify-center min-h-screen mx-auto">
-  <canvas width="600" height="600" bind:this={canvas}></canvas>
+<div bind:this={container} class="container flex flex-col items-center justify-around min-h-screen gap-4 p-4 mx-auto sm:flex-row">
+  <div class="flex flex-col gap-4 p-4 bg-gray-300 rounded-md">
+    <NumberInput label="Maze size" name="size" bind:value={size} min="5" max="50">
+      <input type="range" name="size-range" bind:value={size} min="5" max="50" step="5">
+    </NumberInput>
+
+    <div class="flex justify-end">
+      <button on:click={generate} class="btn">Generate</button>
+    </div>
+
+    <div class="flex items-center gap-2">
+      <input type="checkbox" id="solved" bind:checked={solved}>
+      <label for="solved" class="text-sm">Show solution</label>
+    </div>
+  </div>
+
+  <canvas bind:this={canvas} class="rounded-sm"></canvas>
 </div>
---
+++ src/NumberInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let label: string;
+  export let name: string;
+  export let value: number;
+</script>
+
+<div class="flex flex-col gap-2">
+  <div class="flex items-center gap-4">
+    {#if label}
+      <label for={name} class="text-sm font-semibold uppercase">{label}</label>
+    {/if}
+    
+    <input type="number" {name} id={name} bind:value {...$$restProps} class="w-20 p-1 text-center rounded-md ring-blue-600 focus:ring-2 focus:outline-none">
+  </div>
+
+  <slot/>
+</div>
